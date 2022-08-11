36e23573d09cb7eddcf00fe6bd89be1368b83f9f
MichaelMar7 <michaelmar719@gmail.com>
2022-08-11 15:02:18 +1200

    stuff

--- datareader/csvdatareader.py
+++ datareader/csvdatareader.py
@@ -2,13 +2,12 @@ import os
 import csv
 import ast
 
+from domainmodel.track import Track
 
 class TrackCSVReader:
-
     def __init__(self, albums_csv_file: str, tracks_csv_file: str):
-        # TODO
-        self.__albums_csv_file = None
-        self.__tracks_csv_file = None
+        self.__albums_csv_file = albums_csv_file
+        self.__tracks_csv_file = tracks_csv_file
 
         self.__dataset_of_tracks = []
         # Set of unique artists
@@ -35,5 +34,38 @@ class TrackCSVReader:
         return self.__dataset_of_genres
 
     def read_csv_files(self):
-        # TODO
-        pass
+        with open(self.__tracks_csv_file, encoding="unicode_escape") as file:
+            track_file = csv.DictReader(file)
+            for line in track_file:
+                new_track = Track(int(line["track_id"]), line["track_title"])
+                track_artist = Artist(int(line["artist_id"]), line["artist_name"])
+                new_track.artist = track_artist
+                if line["track_genres"] != "":
+                    track_genre_raw_list = ast.literal_eval(line["track_genres"])
+                    for genre_entry in track_genre_raw_list:
+                        track_genre = Genre(int(genre_entry["genre_id"]), genre_entry["genre_title"])
+                        new_track.genres.append(track_genre)
+                        self.__dataset_of_genres.add(track_genre)
+                new_track.track_duration = int(float(line["track_duration"]))
+                new_track.track_duration = int(float(line["track_duration"]))
+                new_track.track_url = line["track_url"]
+                self.__dataset_of_tracks.append(new_track)
+                self.__dataset_of_artists.add(track_artist)
+                
+        with open(self.__albums_csv_file, encoding="unicode_escape") as file:
+            album_file = csv.DictReader(file)
+            for line in album_file:
+                new_album = Album(int(line["album_id"]), line["album_title"])
+                self.__dataset_of_albums.add(new_album)
+
+
+albums_file_name = 'CS235_S2_2022-Assignment1-CodeSkeleton\data\raw_albums_excerpt.csv'
+tracks_file_name = 'CS235_S2_2022-Assignment1-CodeSkeleton\data\raw_tracks_excerpt.csv'
+reader = TrackCSVReader(albums_file_name, tracks_file_name)
+reader.read_csv_files()
+
+print(f'number of unique tracks: {len(reader.dataset_of_tracks)}')
+print(f'number of unique artists: {len(reader.dataset_of_artists)}')
+print(f'number of unique albums: {len(reader.dataset_of_albums)}')
+print(f'number of unique genres: {len(reader.dataset_of_genres)}')
+
